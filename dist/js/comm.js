580230fa3cce8232e8b9c06ab59ae16d9d034a02
HeejaeDeckyJeong <136055547+HeejaeDeckyJeong@users.noreply.github.com>
2023-08-25 11:36:02 +0900

    bg1

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -24,7 +24,7 @@ $('#fullpage').fullpage({
             });
             $('.header-links').css('background-color', 'transparent');
             $('.logo-w').css('display', 'block');
-            $('.logo-b').css('display', 'none');
+            $('#myMenu').css('display', 'none');
             $('.navTrigger > i').css('background-color', 'white');
         } else if (index != 1) {
             $('.header-links a').each(function () {
@@ -33,7 +33,7 @@ $('#fullpage').fullpage({
             $('.header-links').css('background-color', 'white');
 
             $('.logo-w').css('display', 'none');
-            $('.logo-b').css('display', 'block');
+            $('#myMenu').css('display', 'flex');
             $('.navTrigger > i').css('background-color', 'black');
         }
 
@@ -93,3 +93,33 @@ $(function () {
         }
     });
 });
+// youtube.js
+// comm.js 위에다 추가
+
+// Youtube IFrame API를 비동기로 로드합니다.
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// onYouTubePlayerAPIReady 함수 이름은,
+// Youtube IFrame Player API에서 사용하는 이름이기 때문에,
+// 다르게 지정하면 동작하지 않습니다!
+// 그리고 함수는 전역(Global) 등록해야 합니다!
+function onYouTubePlayerAPIReady() {
+    // <div id="player"></div>
+    new YT.Player('player', {
+        videoId: 'tho0k_VR7kY', // 최초 재생할 유튜브 영상 ID
+        playerVars: {
+            autoplay: true, // 자동 재생 유무
+            loop: true, // 반복 재생 유무
+            playlist: 'tho0k_VR7kY', // 반복 재생할 유튜브 영상 ID 목록
+        },
+        events: {
+            // 영상이 준비되었을 때,
+            onReady: function (event) {
+                event.target.mute(); // 음소거!
+            },
+        },
+    });
+}
